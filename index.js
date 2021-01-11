c038596d0fbc09542a62ac95ef8c5b5d55d80bab
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-10 22:09:52 -0500

    finished looping lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+    return messages
+};
+
+function countDown(num){
+    while (num > 0) {
+        console.log(num);
+        num--;
+    }
+    console.log(num);    
+}; 
